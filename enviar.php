41d63e12687e3437ce68bcb1c8eca15e2c714aa1
BuildTools <unconfigured@null.spigotmc.org>
2015-04-26 18:09:02 +0200

    Versión 0.0.1 [Alpha]
    Versión 0.0.1 [Alpha]

---
+++ enviar.php
@@ -0,0 +1,34 @@
+<?php
+if(isset($_POST['email'])) {
+
+// Debes editar las próximas dos líneas de código de acuerdo con tus preferencias
+$email_to = "tu email";
+$email_subject = "Contacto desde el sitio web";
+
+// Aquí se deberían validar los datos ingresados por el usuario
+if(!isset($_POST['name']) ||
+!isset($_POST['email']) ||
+!isset($_POST['subject']) ||
+!isset($_POST['message'])) {
+
+echo "Ocurrio un error y el formulario no ha sido enviado.";
+echo "Por favor, vuelva atras y verifique la informacion ingresada";
+die();
+}
+
+$email_message = "Detalles del formulario de contacto:\n\n";
+$email_message .= "Nombre: " . $_POST['name'] . "\n";
+$email_message .= "E-mail: " . $_POST['email'] . "\n";
+$email_message .= "Asunto: " . $_POST['subject'] . "\n";
+$email_message .= "Mensaje: " . $_POST['message'] . "\n\n";
+
+
+// Ahora se envía el e-mail usando la función mail() de PHP
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers);
+
+echo "¡El formulario se ha enviado con éxito!";
+}
+?>
